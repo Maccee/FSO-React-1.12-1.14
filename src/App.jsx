c591eb17b0cfcfae20968732ef237f9a00d72607
Macce <makkeeh@gmail.com>
2023-09-28 11:52:55 +0300

    1.13

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,24 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [points, setPoints ] = useState(new Array(anecdotes.length).fill(0));
 
   const random = () => {
     const index = Math.floor(Math.random() * anecdotes.length);
     setSelected(index);
   };
 
+  const vote = () => {
+    const copy = [...points];
+    copy[selected] += 1; 
+    setPoints(copy);
+  };
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p> 
+      <Button onClick={vote} text="vote" />
       <Button onClick={random} text="next anecdote" />
     </div>
   );
